aa85a15b2d8154da5261de306ed0229093bc9a18
Ehan Duan <yxd450@case.edu>
2023-12-05 21:15:18 -0600

    init

---
+++ src/main/java/com/demo/spring3openapi/Spring3openapiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring3openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3openapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring3openapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/spring3openapi/Spring3openapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring3openapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3openapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
